5890183fca0a95d3199696a94fdf8b726d6f6e29
Singla <jssingla@amazon.com>
2016-11-11 10:42:59 +0530

    First Commit for Money

---
+++ app/src/main/java/app/developer/jtsingla/money/EnterActivity.java
@@ -0,0 +1,13 @@
+package app.developer.jtsingla.money;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+}
