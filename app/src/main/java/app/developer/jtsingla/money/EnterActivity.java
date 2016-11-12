cc6b7fa1ca5e1addf67524a1ec8cfdbf0080db84
Singla <jssingla@amazon.com>
2016-11-12 11:17:13 +0530

    Changed background image of enterActivity

--- app/src/main/java/app/developer/jtsingla/money/EnterActivity.java
+++ app/src/main/java/app/developer/jtsingla/money/EnterActivity.java
@@ -2,6 +2,7 @@ package app.developer.jtsingla.money;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class EnterActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class EnterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+    }
+
+    public void enter(View v) {
+
     }
 }
